0e90eda91d8f6543e20ef5763e10851bb28a25c5
kallyas <kallyasmedia@gmail.com>
2024-12-15 18:37:57 +0300

    Configure Next.js to support video processing and add image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["randomuser.me","via.placeholder.com"],
+  },
+};
 
-export default nextConfig;
+export default withNextVideo(nextConfig);
