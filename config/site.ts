7622d4bdf79c1ecf03c570f8b5de4ddb72ea439b
kallyas <kallyasmedia@gmail.com>
2024-12-15 18:52:26 +0300

    Add site configuration for Pearl Mentor Hub with navigation and social links

---
+++ config/site.ts
@@ -0,0 +1,36 @@
+export const siteConfig = {
+    name: "Pearl Mentor Hub",
+    description: "A Sustainable Centre of Excellence for Career Mentorship",
+    mainNav: [
+      {
+        title: "Home",
+        href: "/",
+      },
+      {
+        title: "About",
+        href: "/about",
+      },
+      {
+        title: "Mentors",
+        href: "/mentors",
+      },
+      {
+        title: "Programs",
+        href: "/programs",
+      },
+      {
+        title: "Publications",
+        href: "/publications",
+      },
+      {
+        title: "Pricing",
+        href: "/pricing",
+      },
+    ],
+    links: {
+      twitter: "https://twitter.com/pearlmentorhub",
+      github: "https://github.com/pearlmentorhub",
+    },
+  }
+  
+  
