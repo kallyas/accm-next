6a29de2762a7501e4f65749c20bc5985df60172c
kallyas <kallyasmedia@gmail.com>
2024-12-16 23:19:32 +0300

    Add pagination utility function: implement getPagination for managing page limits

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getPagination(page: number, pageSize: number) {
+  const limit = pageSize
+  const from = page ? page * limit : 0
+  const to = page ? from + pageSize - 1 : pageSize - 1
+
+  return { from, to }
+}
+
