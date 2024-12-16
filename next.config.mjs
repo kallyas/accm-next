0259a1912b47eef9087bf9bec9babc7135503feb
kallyas <kallyasmedia@gmail.com>
2024-12-16 23:14:45 +0300

    Remove video processing integration from Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import { withNextVideo } from "next-video/process";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -6,4 +6,4 @@ const nextConfig = {
   },
 };
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
