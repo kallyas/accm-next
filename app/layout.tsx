dc45cdd11368748c9616f7a56270324398ddf5a2
kallyas <kallyasmedia@gmail.com>
2024-12-17 20:08:05 +0300

    Change default theme to system in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({
         <Providers session={session}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
