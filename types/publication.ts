05686c68504e416a349d0daf72d46cdee6e93e19
kallyas <kallyasmedia@gmail.com>
2024-12-26 22:40:53 +0300

    Add Publication type definition for type safety in publication management

---
+++ types/publication.ts
@@ -0,0 +1,10 @@
+export type Publication = {
+  id: string;
+  title: string;
+  authors: string;
+  abstract: string;
+  externalUrl: string;
+  publishedDate: string;
+  createdAt: string;
+  updatedAt: string;
+};
