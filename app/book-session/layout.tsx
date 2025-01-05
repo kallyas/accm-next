d32d7b8a8cd2152ed70def6cc385b3558b113543
kallyas <kallyasmedia@gmail.com>
2025-01-05 15:54:47 +0300

    Remove login redirect for unauthenticated users in book session layout

--- app/book-session/layout.tsx
+++ app/book-session/layout.tsx
@@ -10,9 +10,5 @@ export default async function BookSessionLayout({
 }) {
   const session = await getServerSession(authOptions);
 
-  if (!session) {
-    redirect(`/login?callbackUrl=${encodeURIComponent("/book-session")}`);
-  }
-
   return children;
 }
