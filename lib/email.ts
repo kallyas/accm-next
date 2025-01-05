fb763ef6acd69c11f30d070ecf781272b28e39c1
kallyas <kallyasmedia@gmail.com>
2025-01-06 00:55:57 +0300

    Remove console log for successful email sending

--- lib/email.ts
+++ lib/email.ts
@@ -19,7 +19,7 @@ export async function sendEmail({ to, subject, text, html }: EmailParams) {
       html: html || text,
     });
 
-    console.log('Email sent successfully:', data);
+    
   } catch (error) {
     console.error('Error sending email:', error);
     throw new Error('Failed to send email');
