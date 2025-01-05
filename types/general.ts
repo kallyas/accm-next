c0f20e2b8523f00601ccd08a787d4ea9077057e6
kallyas <kallyasmedia@gmail.com>
2025-01-05 15:57:04 +0300

    Add PersonalDiscoveryData type for user aspirations and strengths

---
+++ types/general.ts
@@ -0,0 +1,16 @@
+export type PersonalDiscoveryData = {
+  strengths: string[];
+  weaknesses: string[];
+  opportunities: string[];
+  achievements: string[];
+  threats: string[];
+  familyAspirations: string[];
+  careerAspirations: string[];
+  financialBusinessAspirations: string[];
+  socialAspirations: string[];
+  desiredPosition: string[];
+  requiredSkills: string[];
+  coursesAndTrainings: string[];
+  strategies: string[];
+  shortTermGoals: string[];
+};
