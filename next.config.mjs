163b228acaa86ec9a9f58cfe789f7925368b5ae6
kallyas <kallyasmedia@gmail.com>
2025-01-11 17:21:47 +0300

    Add new image domain to next.config for cloudflare storage

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["randomuser.me", "via.placeholder.com"],
+    domains: ["randomuser.me", "via.placeholder.com","accm.8e57be3431cfed150bc5d5643a027333.r2.cloudflarestorage.com"],
     minimumCacheTTL: 60 * 60 * 48, // 48 hours
   },
   eslint: {
