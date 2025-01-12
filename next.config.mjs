841dc607db9f9ec69b0b58c4d64bf83421d4416d
kallyas <kallyasmedia@gmail.com>
2025-01-12 16:38:26 +0300

    Add 'placehold.co' to allowed image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["randomuser.me", "via.placeholder.com","accm.8e57be3431cfed150bc5d5643a027333.r2.cloudflarestorage.com"],
+    domains: [
+      "randomuser.me",
+      "via.placeholder.com",
+      "accm.8e57be3431cfed150bc5d5643a027333.r2.cloudflarestorage.com",
+      "placehold.co",
+    ],
     minimumCacheTTL: 60 * 60 * 48, // 48 hours
   },
   eslint: {
