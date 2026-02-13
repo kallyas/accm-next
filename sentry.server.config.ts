be37a6e2be7eb2fabf1a420f4003971ad77fe32b
kallyas <kallyasmedia@gmail.com>
2026-02-13 10:31:06 +0300

    feat: Initialize Sentry server configuration
    This commit adds a new file `sentry.server.config.ts` to configure Sentry for the server-side environment.
    
    The configuration includes:
    - Setting the DSN for Sentry ingestion.
    - Enabling all traces by setting `tracesSampleRate` to 1.
    - Enabling logs to be sent to Sentry via `enableLogs: true`.
    - Enabling the sending of default Personally Identifiable Information (PII) to Sentry.
    
    Signed-off-by: kallyas <kallyasmedia@gmail.com>

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4092ca50e3f0ccde610662db1b96482b@o4510034856968192.ingest.us.sentry.io/4510877452140544",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
